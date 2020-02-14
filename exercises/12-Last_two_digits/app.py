3ae49614a95248aafaebc69b2174164fcf32f2ca
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-14 14:59:22 +0000

    friday

--- exercises/12-Last_two_digits/app.py
+++ exercises/12-Last_two_digits/app.py
@@ -1,6 +1,7 @@
 #Complete the function to print the last two digits of an interger greater than 9. 
 def last_two_digits(num):
-    
+    print(num % 100)
 
 #Invoke the function with any interger greater than 9.
 
+last_two_digits(32344224789)
--- exercises/13-Tens_digit/app.py
+++ exercises/13-Tens_digit/app.py
@@ -1,8 +1,10 @@
 #Complete the function to print the tens digit of a given interger
 def tens_digit(num):
+    print((num // 10) % 10)
 
 
 
 
 
 #Invoke the function with any interger.
+tens_digit(23875928374982769)
