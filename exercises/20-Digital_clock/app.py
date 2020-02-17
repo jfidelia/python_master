7b89bdfd17678efea073d96e18d178e7763d6c52
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-17 20:09:06 +0000

    exercise2.0

--- exercises/20-Digital_clock/app.py
+++ exercises/20-Digital_clock/app.py
@@ -1,6 +1,7 @@
 #Complete the function to print how many hrs and min are displayed on the 24th digital clock.
 def digital_clock(n):
-
+    print(n // 60, n % 60)
 
 #Invoke the function with any interger (seconds after midnight)
 
+digital_clock(250)
