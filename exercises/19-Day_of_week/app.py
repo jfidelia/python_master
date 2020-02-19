752d73d8ef8692a79efa8f4bf0ca0945ec03e523
Jacques Fidelia Jr <jacques.fidelia@gmail.com>
2020-02-19 02:12:33 +0000

    exercise 19

--- exercises/19-Day_of_week/app.py
+++ exercises/19-Day_of_week/app.py
@@ -1,7 +1,8 @@
 #Complete the function to print the number of day of the week for k'th day of year. 
 def day_of_week(k):
-
+    print((3 + k) % 7)
 
 
 
 #Invoke function day_of_week with an interger between 0 and 6 (number for days of week)
+day_of_week(11)
